2724e3f4ef324e277ef262a6a941823334bc19f5
visavadiyavrushik <180120116126@git.org.in>
2021-05-20 15:45:18 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,100 @@
+import React, { useState } from 'react';
+const api = {
+  key:"5027671a85a6aed2f1ed6f905b84b7a3",
+  base:"https://api.openweathermap.org/data/2.5/"
+}
+
+
+function App() {
+
+  const[query,setQuery] = useState('');
+  const[weather,setWeather] = useState({});
+
+  const search = evt =>{
+    if (evt.key === "Enter") {
+      fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+      .then(res => res.json())
+      .then(reasult => {
+        setWeather(reasult);
+        setQuery(''); 
+        console.log(reasult);
+      });
+    }
+  }
+  
+
+const dateBuilder = (d) =>{
+  let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+  let days = ["sunday","monday","tuesday","wednesday","thursday","friday","saturday"];
+
+  let day = days[d.getDay()];
+  let date = d.getDate();
+  let month = months[d.getMonth()];
+  let yaer = d.getFullYear();
+
+  return `${day} ${date} ${month} ${yaer}`
+}
+
+
+  return (
+
+    <>
+    <div className={(typeof weather.main != "undefined") ? ((weather.main.temp > 16) ? 'app warm' : 'app') : 'app'}>
+      <main>
+        <div className="search-box">
+          <input 
+          type="text"
+          className="search-bar"
+          placeholder="search..."
+          onChange={e => setQuery(e.target.value)}
+          value={query}
+          onKeyPress={search}
+          />
+
+        </div>
+        {(typeof weather.main != "undefined") ? (
+        <div>
+          <div className="location-box">
+            <div className="location">{weather.name},{weather.sys.country}</div>
+            <div className="date">{dateBuilder(new Date())}</div>
+            
+            </div>
+
+            <div className="weather-box">
+              <div className="temp">
+                {Math.round(weather.main.temp)}°c
+              </div>
+             <div className="weather">{
+             weather.weather[0].main}</div>
+             
+              <div className="weather-disc">description: {
+             weather.weather[0].description } </div>
+
+
+            </div>
+        </div>
+            ) : ('')}
+      
+      </main>
+      
+    </div>
+    </>
+  );
+}
+
+export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+// powawal487@botfed.com
+// pass:stark-kelp-widget-arming
